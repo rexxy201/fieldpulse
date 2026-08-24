d730462fb6489609965b2779de4c57547d2525b0
Osawe Ekorowiro <rexxyintegrated@gmail.com>
2026-08-24 12:07:31 +0100

    Add Finance to the Create Team Type dropdown

--- pages/team.php
+++ pages/team.php
@@ -563,6 +563,7 @@ require __DIR__ . '/../includes/header.php';
           <select name="type" class="form-select form-select-sm">
             <option value="fiber">Fiber</option><option value="noc">NOC</option>
             <option value="cx">CX</option><option value="installation">Installation</option>
+            <option value="finance">Finance</option>
           </select>
         </div>
       </div>
